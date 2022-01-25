27de8600d6622ba1f5f277ec3d467cf82e580350
Yasin Munye <cyasinsadiq@gmail.com>
2022-01-25 12:37:08 -0500

    Added questions for manager infor and added file to lib repo with files to each teamate and added starter code to html index files

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
+const fs = require ('fs');
 const inquirer = require('inquirer');
 const jest = require('jest');
+
+function (){
+    console.log("Please Enter Team Manager Information");
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'Name',
+            message: "Enter team manager’s name?"
+        },
+        {
+            type: 'input',
+            name: 'Id',
+            message: "Enter team manager’s employee ID?"
+        },
+        {
+            type: 'input',
+            name: 'Email',
+            message: "Enter team manager’s email?"
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "Enter team manager’s office number?"
+        }
+    ])
